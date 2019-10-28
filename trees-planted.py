1cdc9bbee0203c16b9e36d90a933613437180298
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-10-29 03:11:17 +0530

    Final Update

--- trees-planted.py
+++ trees-planted.py
@@ -13,16 +13,12 @@ ACCESS_SECRET = environ['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-print('API authenticated')
 
 while (True):
     html = urllib.request.urlopen("https://www.teamtrees.org/")
-    print('url opened')
     soup = BS(html, "html.parser")
     count = int(soup.findAll('div', {'id': 'totalTrees'})[0].get('data-count').strip())
-    print(count)
     percent = round(float((count / 20000000) * 100), 1)
-    print(percent)
 
     if percent > previous:
         tweet = str(count) + "/" + "20000000 (" + str(percent) + "%) Trees Planted #TeamTrees teamtrees.org"
