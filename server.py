aa0263f4e7e1ecb0546eb81328d3a34938b4b335
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-10-31 13:52:19 +0530

    Server Fix

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
+def server():
+    return "trees-planted Web Server in Flask"
+    return "Deployed on Heroku\n"
 
-print('trees-planted Web Server in Flask')
-print('Deployed on Heroku\n')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=environ.get('PORT'))
