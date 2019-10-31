c947056fb4be483b1d3c0b5d4a332f3a0af9d6cd
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-10-31 13:40:47 +0530

    Web Server displays message

--- server.py
+++ server.py
@@ -2,4 +2,9 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
+@app.route("/")
+
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
+print('trees-planted Web Server in Flask')
+print('Deployed on Heroku\n')
