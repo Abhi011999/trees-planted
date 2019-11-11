a8c640d6c3286498439d27f07e49fc7ea739954e
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-11-12 03:43:59 +0530

    Added New Reply Feature
    Now bot replies to its followers and following people when their tweets contain "#TeamTrees", with its recent tweet.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # trees-planted
 A Twitter bot that fetches donations metrics from https://www.teamtrees.org/ and converts into percentage to tweet through https://twitter.com/TeamTrees6
 
+## Features
+- Scrapes details of total donation from the https://www.teamtrees.org/ and converts them into percentage and tweets on https://twitter.com/TeamTrees6
+- Replies its recent tweet to current followers and following, when any of their tweets contain "#TeamTrees"
+
 ## Deployment
 On [Heroku](https://www.heroku.com/)
 
--- trees-planted.py
+++ trees-planted.py
@@ -9,9 +9,44 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
+class StreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+
+    def on_status(self, tweet):
+        if (tweet.text.find('#TeamTrees') != -1):
+            print(tweet.user.name, " : ", tweet.text)
+            reply = str("@" + tweet.user.screen_name + " https://twitter.com/TeamTrees6/status/" + api.user_timeline(exclude_replies=True, include_rts=False)[0].id_str)
+            print("Replying to - " + tweet.user.name)
+            print(api.update_status(status=reply, in_reply_to_status_id=tweet.id))
+        return True
+
+    def on_error(self, status_code):
+        print("Got an error with code: ", str(status_code))
+        return True
+
+    def on_timeout(self):
+        print("Timeout...")
+        return True
+
+def get_list(api):
+    bot_followers = api.followers_ids()
+    bot_friends = api.friends_ids() 
+    for i in range(0, len(bot_followers)):
+        bot_followers[i] = str(bot_followers[i])
+    for i in range(0, len(bot_friends)):
+        bot_friends[i] = str(bot_friends[i])
+    final_list = bot_followers + bot_friends
+    return final_list
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+final_list = get_list(api)
+listener = StreamListener(api)
+stream = tweepy.Stream(api.auth, listener)
+stream.filter(follow=final_list, is_async=True)
 
 while (True):
     req = Request("https://www.teamtrees.org/", headers={'User-Agent': 'Mozilla/5.0'})
